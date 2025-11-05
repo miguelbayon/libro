5873df53f77647a76d1c0b7ffcd4475ca05976e8
Miguel Bayon <miguel.bayon@iessanandres.com>
2025-11-05 10:11:39 +0100

    Commit inicial

---
+++ Libro.java
@@ -0,0 +1,11 @@
+
+public class Libro {
+
+    private String autor;
+    private String titulo;
+
+    public Libro(String autorLibro, String tituloLibro) {
+        autor = autorLibro;
+        titulo = tituloLibro;
+    }
+}
